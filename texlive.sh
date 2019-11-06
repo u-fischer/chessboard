fbdd407f642627c37f3963e353f8f5f7f830cad3
Ulrike Fischer <fischer@troubleshooting-tex.de>
2019-11-06 23:09:07 +0100

    and now with a tag

--- texlive.sh
+++ texlive.sh
@@ -7,6 +7,7 @@
 
 # See if there is a cached version of TL available
 export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
+
 if ! command -v texlua > /dev/null; then
   # Obtain TeX Live
   wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
